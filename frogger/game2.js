9397286749663600f7661c7a2eaf4f510f2b13a3
msilverblatt <msilverblatt@gmail.com>
2013-04-01 00:43:00 -0400

    restarted and actually planned it out this time

---
+++ frogger/game2.js
@@ -0,0 +1,414 @@
+sprite = new Image();
+sprite.src = "assets/frogger_sprites.png";
+
+var frog = {};
+frog.direction = "forward";
+
+var vehicles = [];
+var logs = [];
+
+var frog_side = 21;
+var frog_up = 25;
+
+var frog_y = [500,467,432,397,362,327,280,245,215,188,157,126,85];
+
+ 
+var vehicle_types = [];
+vehicle_types[0] = "yellow";
+vehicle_types[1] = "white_car";
+vehicle_types[2] = "purple";
+vehicle_types[3] = "other_white";
+vehicle_types[4] = "truck";
+
+var widths = []
+widths["small"] = 82;
+widths["med"] = 114;
+widths["long"] = 177;
+widths["yellow"] = 23;
+widths["white"] = 24;
+widths["other_white"] = 24;
+widths["truck"] = 47;
+widths["purple"] = 31;
+
+var heights = []
+heights["small"] = 20;
+heights["med"] = 20;
+heights["long"] = 20;
+heights["yellow"] = 23;
+heights["white"] = 26;
+heights["other_white"] = 21;
+heights["truck"] = 18;
+heights["purple"] = 21;
+
+var speeds = [];
+speeds[0] = .5;
+speeds[1] = 1;
+speeds[2] = 2;
+speeds[3] = 3;
+
+var level = 1;
+var score = 0;
+var highscore = 0;
+var lives = 3;
+
+var fps = 33;
+var interv;
+
+var onlog = false;
+var logid;
+
+document.addEventListener("keydown", function(event) {
+    	console.log(event.keyCode);
+    	switch(event.keyCode){
+    		case 37:
+    			event.preventDefault();
+    			move_frog("left");
+    			break;
+
+    		case 38:
+    			event.preventDefault();
+    			move_frog("forward");
+     			break;
+    		case 39:
+    			event.preventDefault();
+    			move_frog("right");
+     			break;
+    		case 40:
+    			event.preventDefault();
+     			move_frog("backward");
+   		 		break;
+    	}
+ 	});
+
+function init_setup(){
+	canvas = document.getElementById("game");
+	ctx = canvas.getContext('2d');	
+	frog.xcor = canvas.width/2 - 12;
+	frog.ycor = 0;
+	setLevel(level);
+	interv = setInterval(run,fps);
+}
+
+function run(){
+	animate();
+	draw();
+	detect_collisions();
+}
+
+function detect_collisions(){
+	for (var v in vehicles){
+		var m = vehicles[v];
+		var x = frog.xcor;
+		var y = frog_y[frog.ycor];
+		var xdiff = x - m.xcor;
+		var ydiff = y - m.ycor;
+		width = widths[m.type];
+		height = heights[m.type];
+		if (Math.abs(xdiff) < width){
+			if (Math.abs(ydiff) < height){
+				die();
+			}
+		}
+
+	}
+	var on = false;
+	for (var l in logs){
+		var m = logs[l];
+		var x = frog.xcor;
+		var y = frog_y[frog.ycor];
+		var xdiff = x - m.xcor;
+		var ydiff = y - m.ycor;
+		width = widths[m.type];
+		height = heights[m.type];
+		if (Math.abs(xdiff) < width){
+			if (Math.abs(ydiff) < height){
+				on = true;
+				if (!onlog){
+					onlog = true;
+					logid = l;
+				} else if (logid != l){
+					logid = l;
+				}
+			}
+		}
+
+	}
+	if (frog.ycor > 6){
+		if (!on){
+			die();
+		}
+	} else {
+		onlog = false;
+	}
+}
+
+function setLevel(level){
+	if (level == 1){
+		for (i = 0; i < 14; i++){
+			vehicles[i] = {};
+			vehicles[i].speed = 2;
+			if ((Math.floor(i / 3) % 2) == 0){
+				vehicles[i].dir = "left";
+				vehicles[i].xcor = (405 + ((i % 3) * 75));
+			} else {
+				vehicles[i].dir = "right";
+				vehicles[i].xcor = (-5 - ((i % 3) * 75));
+			}
+			if (i > 11){
+				vehicles[i].speed = 1;
+			}
+			vehicles[i].ycor = 467 - (Math.floor(i / 3) *  35);
+			vehicles[i].type = vehicle_types[Math.floor(i / 3)];
+			console.log(vehicles[i].type);
+		}
+		for (i = 0; i < 4; i++){
+			logs[i] = {};
+			logs[i].type = "small";
+			logs[i].speed = 1;
+			logs[i].dir = "left";
+			logs[i].xcor = 5 + (i * 100);
+			logs[i].ycor = 126;
+
+		}
+		for (i = 4; i < 8; i++){
+			logs[i] = {};
+			logs[i].type = "med";
+			logs[i].speed = 2;
+			logs[i].dir = "right";
+			logs[i].ycor = 157;
+			logs[i].xcor = 395 - ((i-4) * 125);
+
+		}
+		for (i = 8; i < 10; i++){
+			logs[i] = {};
+			logs[i].speed = 3;
+			logs[i].type = "long";
+			logs[i].dir = "left";
+			logs[i].ycor = 188;
+			logs[i].xcor = 5 + ((i - 8) * 200);
+
+		}
+		for (i = 10; i < 14; i++){
+			logs[i] = {};
+			logs[i].dir = "right";
+			logs[i].type = "small";
+			logs[i].speed = 1;
+			logs[i].ycor = 215;
+			logs[i].xcor = 340 - ((i - 10) * 100);
+
+		}
+		for (i = 14; i < 17; i++){
+			logs[i] = {};
+			logs[i].type = "med";
+			logs[i].dir = "left";
+			logs[i].speed = 2;
+			logs[i].ycor = 245;
+			logs[i].xcor = 5 + ((i - 14) * 125);
+
+		}
+	}
+
+}
+
+// Moving Functions
+
+function move_frog(dir){
+	if (dir == "forward"){
+		frog.ycor++;// -= frog_up;
+	} else if (dir == "backward"){
+		frog.ycor--;// += frog_up;
+	} else if (dir == "left"){
+		frog.xcor -= frog_side;
+	} else if (dir == "right"){
+		frog.xcor += frog_side;
+	}
+	frog.dir = dir;
+}
+
+function animate(){
+	for (var v in vehicles){
+	//	console.log(vehicles[v].xcor,vehicles[v].ycor)
+		if (vehicles[v].dir == "left"){
+			vehicles[v].xcor -= speeds[vehicles[v].speed];
+			if (vehicles[v].xcor <= -200){
+				vehicles[v].xcor = 485;
+			}
+		} else if (vehicles[v].dir == "right"){
+			vehicles[v].xcor += speeds[vehicles[v].speed];
+			if (vehicles[v].xcor > 500){
+				vehicles[v].xcor = -75;
+			}
+		}
+	}
+	for (var l in logs){
+		if (logs[l].dir == "left"){
+			logs[l].xcor -= speeds[logs[l].speed];
+			if (logs[l].xcor <= -180){
+				logs[l].xcor = 550;
+			}
+		} else if (logs[l].dir == "right"){
+			logs[l].xcor += speeds[logs[l].speed];
+			if (logs[l].xcor >= 500){
+				logs[l].xcor = -130;
+			}
+		}
+	}
+	if (onlog){
+		if (logs[logid].dir == "left"){
+			frog.xcor -= speeds[logs[logid].speed];
+		} else if (logs[logid].dir == "right"){
+			frog.xcor += speeds[logs[logid].speed];
+		}
+	}
+}
+
+function die(){
+	clearInterval(interv);
+	lives--;
+	if (lives == 0){
+		gameOver();
+	}
+	else {
+		setLevel(level);
+		frog.xcor = canvas.width/2 - 10;
+		frog.ycor = 0;
+		interv = setInterval(run,fps);
+	}
+}
+
+function gameOver(){
+
+}
+
+// Drawing Functions
+
+function draw(){
+	draw_board();
+	setText();
+	life_frog(lives);
+	draw_vehicles();
+	draw_logs();
+	draw_frog();
+}
+
+function draw_board(){
+
+
+// Blue river
+	ctx.fillStyle="#191970";
+	ctx.fillRect(0,0,400,287);
+
+// FROGGER Header
+	ctx.drawImage(sprite,15,13,319,32,15,15,319,32);
+
+// Goal area
+	ctx.drawImage(sprite,0,54,399,53,0,58,400,53);
+	
+// Black road
+	ctx.fillRect(0,288,400,288);
+//  purple bars
+	ctx.drawImage(sprite,0,119,399,35,0,275,400,35);
+	ctx.drawImage(sprite,0,119,399,35,0,495,400,35);
+
+}
+
+function draw_vehicles(){
+	for (var v in vehicles){
+		var m = vehicles[v];
+		draw_car(m.xcor,m.ycor,m.type);
+	}
+}
+
+function draw_frog(){
+	forward_crouched_frog(frog.xcor, frog_y[frog.ycor]);
+}
+
+function draw_logs(){
+	for (var l in logs){
+		draw_log(logs[l].xcor, logs[l].ycor, logs[l].type);
+	}
+}
+
+function setText(){
+	if (score > highscore) highscore = score;
+	ctx.fillStyle="#00FF00";
+	ctx.font="20px Arial";
+	ctx.fillText("Level "+level, 80,545);
+	ctx.font = "15px Arial";
+	ctx.fillText("Score: "+score,1,559);
+	ctx.fillText("Highscore: "+highscore, 81, 559);
+	
+}
+
+function life_frog(lives){
+	for (i = 0; i < lives; i++){
+		ctx.drawImage(sprite,13,335,18,21,(i * 16),531,13,15);
+	}
+}
+function right_crouched_frog(x,y){
+	ctx.drawImage(sprite,13,335,18,21,x,y,18,21);
+}
+function forward_crouched_frog(x,y){
+
+	ctx.drawImage(sprite,13,369,21,19,x,y,21,19);
+}
+
+function forward_stretched_frog(x,y){
+
+
+}
+
+function draw_log(x,y,type){
+	if (type == "small"){
+		small_log(x,y);
+	} else if (type == "med"){
+		med_log(x,y);
+	} else if (type == "long"){
+		big_log(x,y);
+	}
+}
+
+function med_log(x,y){
+	ctx.drawImage(sprite,10,198,114,20,x,y,114,20);
+}
+
+function small_log(x,y){
+	ctx.drawImage(sprite,10,230,82,20,x,y,82,20);
+}
+
+function big_log(x,y){
+	ctx.drawImage(sprite,7,166,177,20,x,y,177,20);
+
+}
+function draw_car(x,y,type){
+		if (type == "white_car"){
+			white_car(x,y);
+		} else if (type == "other_white"){
+			other_white(x,y);
+		} else if (type == "yellow"){
+			yellow_car(x,y);
+		} else if (type == "purple"){
+			purp_car(x,y);
+		} else if (type == "truck"){
+			truck(x,y);
+		}
+}
+function white_car(x,y){
+	ctx.drawImage(sprite,47,267,24,26,x,y,24,26);
+}
+
+function other_white(x,y){
+	ctx.drawImage(sprite,10,300,24,21,x,y,24,21);
+}
+
+function purp_car(x,y){
+	ctx.drawImage(sprite,9,267,31,21,x,y,31,21);
+}
+
+function truck (x,y){
+	ctx.drawImage(sprite,104,303,47,18,x,y,47,18);
+}
+
+function yellow_car(x,y){
+	ctx.drawImage(sprite,83,265,23,23,x,y,23,23);
+}
